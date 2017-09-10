44381b9b62786980939ab2b8d0b25e2f9c132a2e
Walfie <walfington@gmail.com>
2017-09-10 15:29:01 -0400

    Respond with dummy message

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate prost_derive;
 
+extern crate prost;
 extern crate tk_listen;
 extern crate tk_http;
 extern crate tokio_core;
@@ -10,10 +11,12 @@ mod protobuf;
 
 use futures::{Future, Stream};
 use futures::future::{self, FutureResult};
+use prost::Message;
 use std::time::Duration;
 use tk_http::Status;
 use tk_http::server::{Config, Encoder, EncoderDone, Error, Proto};
 use tk_http::server::buffered::{BufferedDispatcher, Request};
+use tk_http::websocket::Packet;
 use tk_listen::ListenExt;
 use tokio_core::net::TcpListener;
 use tokio_core::reactor::Core;
@@ -24,6 +27,7 @@ fn process_http<S>(req: Request, mut e: Encoder<S>) -> FutureResult<EncoderDone<
         e.add_header("Connection", "upgrade").unwrap();
         e.add_header("Upgrade", "websocket").unwrap();
         e.format_header("Sec-Websocket-Accept", &ws.accept).unwrap();
+        e.format_header("Sec-Websocket-Protocol", "binary").unwrap();
         e.done_headers().unwrap();
         future::ok(e.done())
     } else {
@@ -58,9 +62,47 @@ fn main() {
                 &handle,
                 process_http,
                 |output, input| {
-                    input.forward(output).map(|_| ()).map_err(
-                        |e| eprintln!("{:?}", e),
-                    )
+                    input
+                        .map(|packet: Packet| {
+                            let parsed = if let Packet::Binary(bytes) = packet {
+                                protobuf::RequestMessage::decode(bytes)
+                            } else {
+                                return Packet::Close(
+                                    1003,
+                                    "Only binary data is supported".to_string(),
+                                );
+                            };
+
+                            println!("Parsed: {:?}", parsed);
+
+                            let profile_image = {
+                                "https://avatars0.githubusercontent.com/u/11370525?v=4&s=64"
+                            }.into();
+                            let response = protobuf::ResponseMessage {
+                                data: Some(protobuf::response_message::Data::RaidTweetMessage(
+                                    protobuf::RaidTweetResponse {
+                                        boss_name: "Lv60 ユグドラシル・マグナ".into(),
+                                        raid_id: "ABCD1234".into(),
+                                        screen_name: "walfieee".into(),
+                                        tweet_id: 42069,
+                                        profile_image,
+                                        text: "アイカツ！".into(),
+                                        created_at: 1505071158723,
+                                        language: protobuf::Language::English as i32,
+                                    },
+                                )),
+                            };
+
+                            let mut output_buf = Vec::new();
+                            if let Err(_) = response.encode(&mut output_buf) {
+                                return Packet::Close(1011, "Internal server error".to_string());
+                            };
+
+                            Packet::Binary(output_buf)
+                        })
+                        .forward(output)
+                        .map(|_| ())
+                        .map_err(|e| eprintln!("{:?}", e))
                 },
             );
 
@@ -69,6 +111,8 @@ fn main() {
                 .then(|_| Ok(()))
         })
         .listen(1000);
+
+    println!("Listening on {}", addr);
 
     core.run(done).unwrap();
 }

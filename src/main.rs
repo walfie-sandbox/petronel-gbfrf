7ba357fa5beec7568617152d882b11fd7fa50aed
Walfie <walfington@gmail.com>
2017-09-17 19:15:47 -0400

    Update cache in a separate thread

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@
 extern crate prost_derive;
 #[macro_use]
 extern crate error_chain;
+#[macro_use]
+extern crate futures;
 
 extern crate byteorder;
 extern crate bytes;
 extern crate chrono;
-extern crate futures;
+extern crate futures_cpupool;
 extern crate hyper;
 extern crate hyper_tls;
 extern crate petronel;
@@ -26,13 +28,14 @@ mod codec;
 mod websocket;
 
 use futures::{Future, Stream};
+use futures::future::Either;
 use hyper_tls::HttpsConnector;
 use petronel::{ClientBuilder, Token};
 use petronel::error::*;
 use std::time::Duration;
 use tk_http::server::{Config, Proto};
 use tk_listen::ListenExt;
-use tokio_core::reactor::{Core, Interval};
+use tokio_core::reactor::{Core, Interval, Timeout};
 
 quick_main!(|| -> Result<()> {
     let token = Token::new(
@@ -57,25 +60,50 @@ quick_main!(|| -> Result<()> {
         .build(&handle);
 
     // TODO: Make cache optional
-    let cache = persistence::Cache::new(
+    let cpu_pool = futures_cpupool::CpuPool::new_num_cpus();
+    let (cache_client, cache_worker) = persistence::AsyncCache::new(
+        &cpu_pool,
         env("REDIS_URL")?,
         "petronel_bosses".to_string(),
         Some("bosses".to_string()),
-    )?;
-    let bosses = cache.get_bosses()?;
+    );
+
+    const REDIS_TIMEOUT_SECONDS: u64 = 5;
+    let redis_timeout = Timeout::new(Duration::new(REDIS_TIMEOUT_SECONDS, 0), &handle).unwrap();
+
+    // Wow, timeouts are incredibly annoying to use...
+    let initial_bosses = match core.run(cache_client.get_bosses().select2(redis_timeout)) {
+        Ok(Either::A((bosses, _))) => bosses,
+        Ok(Either::B((_timeout, _))) => {
+            bail!(
+                "could not connect to Redis (timed out after {} seconds)",
+                REDIS_TIMEOUT_SECONDS
+            )
+        }
+        Err(Either::A((err, _))) => Err(err)?,
+        Err(Either::B((_err, _))) => unreachable!(),
+    };
 
     let (petronel_client, petronel_worker) =
         ClientBuilder::from_hyper_client(&hyper_client, &token)
             .with_history_size(10)
             .with_subscriber::<codec::WebsocketSubscriber<tokio_core::net::TcpStream>>()
             .filter_map_message(protobuf::convert::petronel_message_to_bytes)
-            .with_bosses(bosses)
+            .with_bosses(initial_bosses)
             .with_metrics(petronel::metrics::simple(
                 |ref m| serde_json::to_vec(&m).unwrap(),
             ))
             .build();
 
-    let config = Config::new().done();
+    // Flush cache every 3 minutes
+    let cache_petronel_client = petronel_client.clone();
+    let cache_flush = Interval::new(Duration::new(30, 0), &core.handle())
+        .unwrap()
+        .then(|r| r.chain_err(|| "failed to create Interval"))
+        .and_then(move |_| cache_petronel_client.export_metadata())
+        .for_each(move |data| Ok(cache_client.update_bosses(data)))
+        .then(|r| r.chain_err(|| "cache flush failed"))
+        .join(cache_worker);
 
     // Send heartbeat every 30 seconds
     let heartbeat_petronel_client = petronel_client.clone();
@@ -84,6 +112,7 @@ quick_main!(|| -> Result<()> {
         .for_each(move |_| Ok(heartbeat_petronel_client.heartbeat()))
         .then(|r| r.chain_err(|| "heartbeat failed"));
 
+    let http_config = Config::new().done();
     let http_websocket_server = listener
         .incoming()
         .sleep_on_error(Duration::from_millis(1000), &handle)
@@ -93,7 +122,7 @@ quick_main!(|| -> Result<()> {
                 petronel_client: petronel_client.clone(),
             };
 
-            Proto::new(socket, &config, dispatcher, &handle)
+            Proto::new(socket, &http_config, dispatcher, &handle)
                 .map_err(|e| eprintln!("Connection error: {}", e))
                 .then(|_| Ok(()))
         })
@@ -102,8 +131,11 @@ quick_main!(|| -> Result<()> {
 
     println!("Listening on {}", bind_address);
 
-    core.run(http_websocket_server.join3(petronel_worker, heartbeat))
-        .chain_err(|| "stream failed")?;
+    core.run(http_websocket_server.join4(
+        petronel_worker,
+        heartbeat,
+        cache_flush,
+    )).chain_err(|| "stream failed")?;
 
     Ok(())
 });
--- src/persistence.rs
+++ src/persistence.rs
@@ -1,4 +1,7 @@
 use chrono::{DateTime, NaiveDateTime, Utc};
+use futures::{self, Async, Future, Stream};
+use futures::sync::{mpsc, oneshot};
+use futures_cpupool::{CpuFuture, CpuPool};
 use petronel::error::*;
 use petronel::model::{BossImageUrl, RaidBoss, RaidBossMetadata};
 use prost::Message;
@@ -7,29 +10,93 @@ use redis::{self, Commands};
 use serde_json;
 use std::collections::HashSet;
 
-pub struct Cache {
+enum CacheMessage {
+    Get(oneshot::Sender<Result<Vec<RaidBossMetadata>>>),
+    Update(Vec<RaidBossMetadata>),
+}
+
+pub struct AsyncCacheClient(mpsc::UnboundedSender<CacheMessage>);
+impl AsyncCacheClient {
+    pub fn get_bosses(&self) -> Box<Future<Item = Vec<RaidBossMetadata>, Error = Error>> {
+        let (tx, rx) = oneshot::channel();
+        let _ = self.0.unbounded_send(CacheMessage::Get(tx));
+
+        Box::new(rx.map_err(|_| "failed to get cached bosses").flatten())
+    }
+
+    pub fn update_bosses(&self, bosses: Vec<RaidBossMetadata>) {
+        let _ = self.0.unbounded_send(CacheMessage::Update(bosses));
+    }
+}
+
+pub struct AsyncCache {
+    cache: Cache,
+    receiver: mpsc::UnboundedReceiver<CacheMessage>,
+}
+
+impl AsyncCache {
+    pub fn new(
+        pool: &CpuPool,
+        url: String,
+        bosses_key: String,
+        legacy_bosses_key: Option<String>,
+    ) -> (AsyncCacheClient, CpuFuture<(), Error>) {
+        let (sender, receiver) = mpsc::unbounded();
+
+        let lazy = futures::future::lazy(move || -> Result<AsyncCache> {
+            let redis_connection = redis::Client::open(url.as_ref())
+                .chain_err(|| "failed to create Redis client")?
+                .get_connection()
+                .chain_err(|| "failed to get Redis connection")?;
+
+            let cache = Cache {
+                redis_connection,
+                bosses_key,
+                legacy_bosses_key,
+            };
+
+            Ok(AsyncCache { cache, receiver })
+        });
+
+        let cpu_future = pool.spawn(lazy.flatten());
+
+        (AsyncCacheClient(sender), cpu_future)
+    }
+}
+
+impl Future for AsyncCache {
+    type Item = ();
+    type Error = Error;
+
+    fn poll(&mut self) -> ::std::result::Result<Async<Self::Item>, Self::Error> {
+        use self::CacheMessage::*;
+        loop {
+            let polled = self.receiver.poll().map_err(|()| "failed to poll cache requests stream");
+            match try_ready!(polled) {
+                Some(Get(sender)) => {
+                    let _ = sender.send(self.cache.get_bosses());
+                }
+                Some(Update(bosses)) => {
+                    if let Err(e) = self.cache.save_bosses(&bosses) {
+                        eprintln!("failed to save to cache: {:?}", e)
+                    }
+                }
+                None => {
+                    return Ok(Async::Ready(()));
+                }
+            }
+        }
+    }
+}
+
+
+struct Cache {
     redis_connection: redis::Connection,
     bosses_key: String,
     legacy_bosses_key: Option<String>,
 }
 
 impl Cache {
-    pub fn new<U>(url: U, bosses_key: String, legacy_bosses_key: Option<String>) -> Result<Self>
-    where
-        U: AsRef<str>,
-    {
-        let redis_connection = redis::Client::open(url.as_ref())
-            .chain_err(|| "failed to start Redis client")?
-            .get_connection()
-            .chain_err(|| "failed to get Redis connection")?;
-
-        Ok(Cache {
-            redis_connection,
-            bosses_key,
-            legacy_bosses_key,
-        })
-    }
-
     pub fn save_bosses(&self, bosses: &[RaidBossMetadata]) -> Result<()> {
         let json = serde_json::to_vec(bosses).chain_err(
             || "failed to serialize boss data to cache",
